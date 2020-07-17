9951fb48b7e155cfe217bc60177b0a1100250ce1
Kapadokia-Titus <ktnyandoch@gmail.com>
2020-07-18 01:18:19 +0300

    starter code

---
+++ app/src/main/java/kapadokia/nyandoro/videoapp/MainActivity.java
@@ -0,0 +1,14 @@
+package kapadokia.nyandoro.videoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
